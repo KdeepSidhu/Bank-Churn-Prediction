c9d7903550dbd37861c2b3715d5c1b92dc28f2fb
KdeepSidhu <64827508+KdeepSidhu@users.noreply.github.com>
2021-06-12 11:11:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bank-Churn-Prediction
+ End to end churn prediction project with feature engineering.
---
+++ churn_web/app/views.py
@@ -0,0 +1,38 @@
+from django import http
+from django.shortcuts import render
+import numpy as np
+import pickle
+import os
+
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+static = os.path.join(BASE_DIR, "static")
+
+def home(request):
+    if request.method =='POST':
+        one = request.POST['1']
+        two = request.POST['2']
+        three = request.POST['3']
+        four = request.POST['4']
+        five = request.POST['5']
+        six = request.POST['6']
+        seven = request.POST['7']
+        eight = request.POST['8']
+        nine = request.POST['9']
+        ten = request.POST['10']
+        feature = [one,two,three,four,five,six,seven,eight,nine,ten]
+        feature = np.array(feature)
+        feature = feature.reshape(1,10)
+        scaler = pickle.load(open(static+'/scaler.sav','rb'))
+        feature = scaler.transform(feature)
+        model = pickle.load(open(static+'/XGB_model.sav','rb'))
+        pred =  model.predict(feature)
+        print(pred)
+        context = {
+            'output': pred[0]
+        }
+
+
+    return(render(request,'home.html',context))
+# def home(request):
+#     return(HttpResponse('m here'))
